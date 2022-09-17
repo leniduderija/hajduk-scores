bd174503fc9223f6df87cba0f2e5edae2c4fcb9c
Leni Đuderija <leni@laplacian.hr>
2022-09-17 16:03:18 +0200

    Removed type

--- apps/hajduk-scores/pages/api/fixtures/[fid]/index.ts
+++ apps/hajduk-scores/pages/api/fixtures/[fid]/index.ts
@@ -1,10 +1,9 @@
 import prisma from '../../../../common/db/prisma';
-import { ApiFixture } from '@hajduk-scores/api-interfaces';
 
 export default async function handle(req, res) {
   const { fid } = req.query;
 
-  const fixtures: Omit<ApiFixture, 'user'>[] = await prisma.fixture.findMany({
+  const fixtures = await prisma.fixture.findMany({
     where: {
       userId: parseInt(fid),
     },
