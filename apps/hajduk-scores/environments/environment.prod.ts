4b31effd3d2a30f0f1f078b5a29b80efed4c156c
Leni Đuderija <leni@laplacian.hr>
2022-09-17 12:29:10 +0200

    Changed production env file

--- apps/hajduk-scores/environments/environment.prod.ts
+++ apps/hajduk-scores/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// When building for production, this file is replaced with `environment.prod.ts`.
+
 export const environment = {
-  production: true,
+  production: false,
+  // apiFootballApiKey: '18f291d4915b76fdaf6eeb3bb134c074',
+  // apiFootballSportsKey: 'https://v3.football.api-sports.io/',
+  // apiFootballBaseUrl: 'https://v3.football.api-sports.io/',
+  // apiBaseUrl: 'https://v3.football.api-sports.io/',
+  // sofaScoreApiBaseUrl: 'https://api.sofascore.com/api/v1',
+  // FAUNADB_SECRET: 'fnAErzf5QoAAzftpcr6bf8dgKD-nTWSRltP26FXk',
+  rapidApiFootballKey: 'e7703c035emshc28dbbe0b8b572cp11b432jsnb3522c900799',
+  rapidApiFootballHost: 'api-football-v1.p.rapidapi.com',
+  rapidApiFootballBaseUrl: 'https://api-football-v1.p.rapidapi.com/v3/',
+  herokuPostgresUrl:
+    'postgres://crqpgbgwxiiirb:942e54c08a07e0167b5e936dd28f5c44962cf990e578d27662e97679290c5d83@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/dfpbm30reqsbt5',
 };
