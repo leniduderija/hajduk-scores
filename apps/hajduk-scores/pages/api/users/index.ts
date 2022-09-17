0649c2a12fa7f165eddf653993b362eba9d9862a
Leni Đuderija <leni@laplacian.hr>
2022-09-17 12:49:27 +0200

    testing users endpoint

--- apps/hajduk-scores/pages/api/users/index.ts
+++ apps/hajduk-scores/pages/api/users/index.ts
@@ -1,10 +1,10 @@
 import prisma from '../../../common/db/prisma';
 
 export default async function handle(req, res) {
-  const users = await prisma.user.findMany({
-    orderBy: {
-      name: 'desc',
-    },
-  });
-  res.json(users);
+  // const users = await prisma.user.findMany({
+  //   orderBy: {
+  //     name: 'desc',
+  //   },
+  // });
+  res.json([]);
 }
